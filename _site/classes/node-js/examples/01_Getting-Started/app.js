bb7cf3e3b4027b5f51d7f615eeb6e15185e49764
Shawn Allen <shawn.allen@hush.com>
2014-07-27 13:21:47 -0700

    no _site dir

--- _site/classes/node-js/examples/01_Getting-Started/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * 01 - Getting Started
- * Declare a variable, perform some math, output the result
- */
-
-var initialNumber = 0;
-var finalNumber = initialNumber + 10;
-
-console.log(finalNumber);
--- _site/classes/node-js/examples/05_Filesystem/stdin_copycat.js
+++
@@ -1,5 +0,0 @@
-/*
- * STDIN Copycat
- * What do you think this code does?
- */
-process.stdin.pipe(process.stdout);
