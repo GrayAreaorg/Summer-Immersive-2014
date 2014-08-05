eb860f22984a905c6e034e4a44fc06a011e5041d
Ben McChesney <ben.mcchesney@gmail.com>
2014-08-04 20:05:48 -0700

    Added class project files to repo.

---
+++ class_materials/wk6 - openFrameworks/04_fragShader_fun/src/ofApp.cpp
@@ -0,0 +1,87 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    //Load the external files for our shader program.
+    //Vertex shaders handle vertices and generating geometry
+    //Fragment shaders focus on manipulating rasterized pixels 
+    shader.load( "basic.vert" , "HexTunnel.frag" ) ;
+    
+    panel.setup("04 Frag Shaders Panel" ) ;
+    panel.add( shaderVariable.set( "var" , 0.5f , 0.0f , 1.0f ) ) ;
+    
+    
+    fbo.allocate( ofGetWidth() , ofGetHeight() ) ;
+    fbo.begin() ;
+        ofClear( 0 , 0 , 0 , 1 ) ;
+    fbo.end() ;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+
+    ofSetColor(255, 255, 255);
+    //Try adding new variables and see how it effects the visuals
+    shader.begin() ;
+    shader.setUniform1f("time", ofGetElapsedTimef() ) ;
+    shader.setUniform2f("resolution", ofGetWidth()  , ofGetHeight() );
+    
+    ofSetColor( 255 ) ;
+    ofRect( 0 , 0 , ofGetWidth() , ofGetHeight() ) ;
+    shader.end() ;
+    
+    panel.draw() ; 
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
